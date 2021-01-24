c629bd48cb437f5ef7c744d524677efca19f8156
Martha Sokolska <pionaiki@gmail.com>
2021-01-24 14:12:03 +0100

    create UI

--- crscript.js
+++ crscript.js
@@ -1,10 +1,21 @@
 function getInput() {
   var input = '';
   input += document.getElementById('MyName').value + '--';
-  input += document.getElementById('MyBirth').value + '--';
+  input += document.getElementById('MyBirth').value;
+  if (document.getElementById('MyBirthTime').value) {
+    input += 'T' + document.getElementById('MyBirthTime').value;
+  }
+  input += '--';
   input += document.getElementById('TheirName').value + '--';
-  input += document.getElementById('TheirBirth').value + '--';
+  input += document.getElementById('TheirBirth').value;
+  if (document.getElementById('TheirBirthTime').value) {
+    input += 'T' + document.getElementById('TheirBirthTime').value;
+  }
+  input += '--';
   input += document.getElementById('Meeting').value;
+  if (document.getElementById('MeetingTime').value) {
+    input += 'T' + document.getElementById('MeetingTime').value;
+  }
   return input;
 }
 
@@ -20,4 +31,14 @@ function copy(object) {
   object.select();
   object.setSelectionRange(0, object.value.length);
   document.execCommand('copy');
+  document.getElementById('Link').value = 'Copied!'
+  setTimeout(function () { update(); }, 1000);
+}
+
+function getTheirName() {
+  if (document.getElementById('TheirName').value) {
+    return document.getElementById('TheirName').value;
+  } else {
+    return 'them';
+  }
 }
